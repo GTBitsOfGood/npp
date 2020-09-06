a29d324014a3f6b2ec6d709e34a42097ccb24fde
Josh-McFarlin <jmcfarlin6@gatech.edu>
2020-09-05 23:41:24 -0400

    Fix Mongo type and make sure Next builds succeed

--- server/mongodb/index.ts
+++ server/mongodb/index.ts
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
-export default async () => {
+export default async (): Promise<void> => {
   if (mongoose.connections[0].readyState) return;
 
   await mongoose
-    .connect(process.env.DATABASE_URL, {
+    .connect(process.env.DATABASE_URL as string, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,

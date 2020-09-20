aa335cd88ee8e95cd0a76bb92ec706a4beba9f24
Josh-McFarlin <jmcfarlin6@gatech.edu>
2020-09-20 17:25:57 -0400

    Remove build from pre-push commit hook for speed and format code

--- server/mongodb/models/Meeting.js
+++ server/mongodb/models/Meeting.js
@@ -1,29 +1,29 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const { Schema } = mongoose;
 
-
 const MeetingSchema = new Schema({
-    interviewer: {
-        type: Schema.Types.ObjectId,
-        required: true,
-    },
-    startDatetime: {
-        type: Date,
-        required: true,
-    },
-    nonprofit: {
-        type: Schema.Types.ObjectId,
-        required: true,
-    },
-    contactName: {
-        type: String,
-        required: true,
-    },
-    contactPhone: {
-        type: String,
-        required: true,
-    }
+  interviewer: {
+    type: Schema.Types.ObjectId,
+    required: true,
+  },
+  startDatetime: {
+    type: Date,
+    required: true,
+  },
+  nonprofit: {
+    type: Schema.Types.ObjectId,
+    required: true,
+  },
+  contactName: {
+    type: String,
+    required: true,
+  },
+  contactPhone: {
+    type: String,
+    required: true,
+  },
 });
 
-export default mongoose.models.Meeting || mongoose.model('Meeting', MeetingSchema);
+export default mongoose.models.Meeting ||
+  mongoose.model("Meeting", MeetingSchema);

5de659cda8ff515a19c29e6c88907b0b119aed8a
Khusbu Patel <khusbu0514@gmail.com>
2020-09-24 12:24:38 -0400

    changes meeting schema to ts

--- server/mongodb/Availability.ts
+++ server/mongodb/Availability.ts
@@ -3,23 +3,23 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const AvailabilitySchema = new Schema({
-    interviewer: {
-        type: Schema.Types.ObjectId,
-        required: true,
-    },
-    startDatetime: {
-        type: Date,
-        required: true,
-    },
-    endDatetime: {
-        type: Date,
-        required: true,
-    },
-    isBooked: {
-        type: Boolean,
-        default: false,
-    }
-})
+  interviewer: {
+    type: Schema.Types.ObjectId,
+    required: true,
+  },
+  startDatetime: {
+    type: Date,
+    required: true,
+  },
+  endDatetime: {
+    type: Date,
+    required: true,
+  },
+  isBooked: {
+    type: Boolean,
+    default: false,
+  },
+});
 
 export default mongoose.models.Availability ||
   mongoose.model("Availability", AvailabilitySchema);

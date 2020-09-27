4da2b8aecab19e8deddcecac12f1544e9027c4d6
Maria Shapiro <mariaashapiro@gmail.com>
2020-09-27 14:05:07 -0400

    added type to Button's props and {...rest}

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,15 @@
 import React from "react";
+import clsx from "clsx";
 import classes from "./Button.module.scss";
 
 interface PropTypes extends React.HTMLProps<HTMLButtonElement> {
   variant?: "primary" | "secondary" | "default";
 }
 
-const Button = ({ children, variant, ...rest }: PropTypes) => (
-  <button className={classes.container}>{children}
+const Button = ({ children, type, variant, ...rest }: PropTypes) => (
+  <button className={clsx(classes.container, variant === "secondary" && classes.secondaryContainer)}  {...rest} >
+    {children}
   </button>
 );
+
 export default Button;

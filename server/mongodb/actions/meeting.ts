7e4d16d7055e0f07a361798fe8fd269f71d14bbf
Khusbu Patel <khusbu0514@gmail.com>
2020-10-18 13:31:37 -0400

    #38 create meeting actions

---
+++ server/mongodb/actions/meeting.ts
@@ -0,0 +1,27 @@
+import { connectToDB } from "../index";
+import MeetingSchema from "../Meeting";
+import { ObjectId } from "mongodb";
+
+export async function addMeeting(meeting: Document) {
+  await connectToDB();
+
+  return MeetingSchema.create(meeting);
+}
+
+export async function getMeetings() {
+  await connectToDB();
+
+  return MeetingSchema.find().sort({ startDateTime: -1 });
+}
+
+export async function getMeetingById(id: ObjectId) {
+  await connectToDB();
+
+  return MeetingSchema.findOne({ _id: id });
+}
+
+export async function getMeetingByApplication(id: ObjectId) {
+  await connectToDB();
+
+  return MeetingSchema.findOne({ _application: id });
+}

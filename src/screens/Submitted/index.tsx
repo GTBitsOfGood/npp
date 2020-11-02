99de1d0f0c0bc6a908af8062c9ae9eb2127eb579
landonhut <landonhut@gmail.com>
2020-11-02 00:42:33 -0500

    Removed router and session #33

--- src/screens/Submitted/index.tsx
+++ src/screens/Submitted/index.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { useRouter } from "next/router";
-import { useSession } from "next-auth/client";
 
 // Components
 import Statusbar from "&components/Statusbar";
@@ -9,14 +7,8 @@ import Statusbar from "&components/Statusbar";
 import classes from "./SubmittedScreen.module.scss";
 
 const SubmittedScreen: React.FC = () => {
-  const router = useRouter();
-  const [session, loading] = useSession();
   const message =
     "Your application has been submitted to the BoG team successfully! You will get an email notification after we finish reviewing your application. If we decide to move on with your project, the next step will be an interview to better understand your project and see if it’s a good fit for Bits of Good.";
-
-  if (loading) {
-    return <h1>Loading...</h1>;
-  }
 
   return (
     <div className={classes.root}>

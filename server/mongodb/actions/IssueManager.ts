cc2375bb0fe65210649e8547bead522a97894e44
Navbryce <plunkett.bryce.m@gmail.com>
2020-11-05 10:00:27 -0500

    68-Export functions in manager. Update return type

--- server/mongodb/actions/IssueManager.ts
+++ server/mongodb/actions/IssueManager.ts
@@ -2,7 +2,7 @@ import IssueDocument from "&server/mongodb/IssueDocument";
 import { connectToDB, EntityDoc } from "../index";
 import { ObjectId } from "mongodb";
 
-async function createIssue(issue: Record<string, any>) {
+async function createIssue(issue: Record<string, any>): Promise<EntityDoc> {
   await connectToDB();
   return IssueDocument.create(issue);
 }
@@ -16,3 +16,9 @@ async function getIssueById(id: ObjectId): Promise<EntityDoc> {
   await connectToDB();
   return IssueDocument.findById(id);
 }
+
+export default {
+  createIssue,
+  getIssues,
+  getIssueById,
+};

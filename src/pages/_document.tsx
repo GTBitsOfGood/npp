2e2e5eabe32b37c6e3313b5bfb5c4b0bf6e85e3e
Josh-McFarlin <jmcfarlin6@gatech.edu>
2020-11-12 23:06:37 -0500

    Add Open Sans font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -24,8 +24,9 @@ class MyDocument extends Document {
           <link rel="manifest" href="/manifest.webmanifest" />
           <meta name="theme-color" content="#f38d69" />
           <link rel="apple-touch-icon" href="/static/logo-apple.png" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&family=Raleway:wght@300;400;500;600;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700;800&display=swap"
             rel="stylesheet"
           />
         </Head>

0ef1f8cc6e2e950136d709b2a77bd47072d7442c
Christopher Jeffrey Obando <chrisjobando@gmail.com>
2020-11-15 15:18:36 -0500

    feat: sidebar on mobile

---
+++ src/components/icons/MenuIcon.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const MenuIcon = () => (
+  <svg
+    width="48"
+    height="48"
+    fill="none"
+    viewBox="0 0 48 48"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fill="#333"
+      d="M7.2002 26.4H40.8002V21.6H7.2002V26.4ZM7.2002 38.4H40.8002V33.6H7.2002V38.4ZM7.2002 9.59998V14.4H40.8002V9.59998H7.2002Z"
+    />
+  </svg>
+);
+
+export default MenuIcon;

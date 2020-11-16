67282f20c2e4aa45e494d4abc55d946944bb9106
Josh-McFarlin <jmcfarlin6@gatech.edu>
2020-11-16 10:59:52 -0500

    Change app index redirect for performance

--- src/screens/App/Project/index.tsx
+++ src/screens/App/Project/index.tsx
@@ -1,6 +1,5 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { GetServerSideProps } from "next";
-import { useRouter } from "next/router";
 import { getSession } from "next-auth/client";
 
 // Components
@@ -11,61 +10,41 @@ import ButtonLink from "&components/ButtonLink";
 import ApplyNewBulb from "&icons/ApplyNewBulb";
 
 // Utils
-import { useSession } from "&utils/auth-utils";
 import urls, { getApplicationUrl } from "&utils/urls";
 
-interface PropTypes {
-  organizationVerified: boolean;
-}
+// interface PropTypes {
+//   organizationVerified: boolean;
+// }
 
-const ProjectPage = ({ organizationVerified }: PropTypes) => {
-  const router = useRouter();
-  const [session, loading] = useSession();
+const ProjectPage = () => (
+  <div className="landingPage">
+    <h1 className="landingHeader">Apply for a New Project</h1>
 
-  useEffect(() => {
-    if (!loading) {
-      if (!session) {
-        void router.replace(urls.pages.index);
-      } else if (!organizationVerified) {
-        void router.replace(urls.pages.app.verification);
-      }
-    }
-  }, [loading, session]);
+    <Statusbar application={null} />
 
-  if (loading || !session) {
-    return <h1 className="loadingText">Loading...</h1>;
-  }
+    <ApplyNewBulb className="landingImage" />
 
-  return (
-    <div className="landingPage">
-      <h1 className="landingHeader">Apply for a New Project</h1>
+    <div className="landingContent">
+      <div className="landingPadding" />
 
-      <Statusbar application={null} />
+      <h3 className="landingText">
+        As a partner, Bits of Good will help you build software that turns your
+        need into real productLorem ipsum dolor sit amet, consectetur adipiscing
+        elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+        ut
+      </h3>
 
-      <ApplyNewBulb className="landingImage" />
-
-      <div className="landingContent">
-        <div className="landingPadding" />
-
-        <h3 className="landingText">
-          As a partner, Bits of Good will help you build software that turns
-          your need into real productLorem ipsum dolor sit amet, consectetur
-          adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-          ullamco laboris nisi ut
-        </h3>
-
-        <div className="landingPadding" />
-      </div>
-
-      <div className="landingButton">
-        <ButtonLink variant="primary" href={urls.pages.app.application.apply}>
-          <h3>Apply Now</h3>
-        </ButtonLink>
-      </div>
+      <div className="landingPadding" />
     </div>
-  );
-};
+
+    <div className="landingButton">
+      <ButtonLink variant="primary" href={urls.pages.app.application.apply}>
+        <h3>Apply Now</h3>
+      </ButtonLink>
+    </div>
+  </div>
+);
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
@@ -77,7 +56,13 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     const session = await getSession({ req: context.req as any });
 
     if (session?.user == null) {
-      throw new Error("User is not logged in!");
+      return {
+        props: {},
+        redirect: {
+          destination: urls.pages.index,
+          permanent: false,
+        },
+      };
     }
 
     const user = await UserManager.getUserById((session.user as any).id);
@@ -96,11 +81,24 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
       };
     }
 
+    const organizationVerified =
+      user.organization != null && user.organization.organizationName != null;
+
+    if (!organizationVerified) {
+      return {
+        props: {
+          organizationVerified,
+        },
+        redirect: {
+          destination: urls.pages.app.verification,
+          permanent: false,
+        },
+      };
+    }
+
     return {
       props: {
-        organizationVerified:
-          user.organization != null &&
-          user.organization.organizationName != null,
+        organizationVerified,
       },
     };
   } catch (error) {

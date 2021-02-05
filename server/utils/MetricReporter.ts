64071614228ff0f71bc65ff69b77c73d1d307886
Navbryce <plunkett.bryce.m@gmail.com>
2021-02-05 11:44:26 -0500

    102-Enable logging out. Added some metrics to investigate what is causing loading issues in vercel. Tweaked how user session was fetched on one page.

---
+++ server/utils/MetricReporter.ts
@@ -0,0 +1,39 @@
+/**
+ * Kind of a dummy class in case we want to add more reporting functionality
+ * later
+ */
+
+export class MetricReporter {
+  public reportIntervalEventInitiated(
+    source: string,
+    event: string,
+    startTime: Date
+  ) {
+    this.reportEvent(
+      source,
+      event,
+      `STARTED [startTime=${startTime.toISOString()}]`
+    );
+  }
+
+  public reportIntervalEventCompleted(
+    source: string,
+    event: string,
+    startTime: Date,
+    endTime: Date = new Date()
+  ) {
+    this.reportEvent(
+      source,
+      event,
+      `COMPLETED [endTime=${endTime.toISOString()}] [Duration=${
+        endTime.getTime() - startTime.getTime()
+      }ms]`
+    );
+  }
+
+  private reportEvent(source: string, event: string, eventMessage: string) {
+    if (parseInt(process.env.DEBUG ?? "0")) {
+      console.log(`${source}: ${event} ${eventMessage}`);
+    }
+  }
+}

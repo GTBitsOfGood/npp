e4cb5bfbc33cf61b857143729f04351bd528cbf8
Nicholas Boshart <nichabosh.code@gmail.com>
2021-02-16 22:16:28 -0500

    fix: rewrote site's pagination logic
    * integrated redirect logic in src/pages/index.tsx
       - used next-auth session and logged-in user's mongodb
         document to determine corresponding redirection page
    * changed auth0 callback function from "/app" to "/"

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,50 @@
 import IndexPage from "&screens/Index";
 
-export default IndexPage;
+import { useSession } from "next-auth/client";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+import urls from "&utils/urls";
+
+const Index = () => {
+  const router = useRouter();
+  const [session] = useSession();
+  const [route, setRoute] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // @ts-ignore
+    if (session && session.user.organizationVerified) {
+      setRoute(urls.pages.app.index);
+      setLoading(false);
+    } else if (session) {
+      const query = new URLSearchParams({
+        email: session.user.email,
+      });
+      fetch(`${urls.baseUrl}${urls.api.user}/?${query}`, {
+        method: "get",
+        mode: "same-origin",
+        credentials: "include",
+      }).then(async (response) => {
+        const json = await response.json();
+        console.log(json.payload.organization);
+        setRoute(
+          json.payload.organization
+            ? urls.pages.app.index
+            : urls.pages.app.verification
+        );
+        setLoading(false);
+      });
+    }
+  }, [session]);
+
+  if (!session) return <IndexPage />;
+  else if (!loading) void router.replace(route);
+
+  // in the event there exists no session (i.e. user isn't
+  // currently logged in), loading text will be displayed
+  // until the correct page is determined and re-routed
+  return <h1 className="loadingText">Loading...</h1>;
+};
+
+export default Index;

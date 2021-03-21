01a21ed075a06f4c148b2b47a510b355ff5d1f10
Saurav Ghosal <sghosal9@gatech.edu>
2021-03-21 16:28:18 -0400

    front end mostly set up -- needs admin verification and data fetching

--- src/screens/Admin/Project/index.tsx
+++ src/screens/Admin/Project/index.tsx
@@ -1,22 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Project = () => {
+  const tableData = useState([]);
+
+  const handleTabClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    // query database here for new info to populate table
+    e.preventDefault();
+  };
+
   return (
-    <div className="landingPage">
-      this is the admin project page
+    <div className="adminLandingPage">
+      <h2 className="landingPageTitle">Project Application</h2>
+      <div className="tabs">
+        <button onClick={handleTabClick}>New</button>
+        <button>In Progress</button>
+        <button>Closed</button>
+      </div>
       <div className="tableContainer">
-        <h2 className="tableText">Maintenance History</h2>
         <table>
           <thead>
             <tr>
-              <th>#</th>
-              <th>ISSUE TYPE</th>
-              <th>DESCRIPTION</th>
-              <th>START DATE</th>
-              <th>END DATE</th>
+              <th>ORGANIZATION</th>
+              <th>EIN</th>
+              <th>ADDRESS</th>
+              <th>DATE SUBMITTED</th>
               <th>STATUS</th>
             </tr>
           </thead>
+          <tbody>
+            <tr>
+              <td>Bits of Good</td>
+              <td>00-00000</td>
+              <td>
+                <div>Atlanta, Georgia</div>
+                <div className="streetName">100 northside dr. nw</div>
+              </td>
+              <td>01/03/2020</td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>Liv2BGirl</td>
+              <td>00-00000</td>
+              <td>
+                <div>Atlanta, Georgia</div>
+                <div className="streetName">100 northside dr. nw</div>
+              </td>
+              <td>01/03/2020</td>
+              <td>
+                <span className="status">New</span>
+              </td>
+            </tr>
+          </tbody>
         </table>
       </div>
     </div>

d22a1c5070db14d4e95c97900f645052dd1106e3
Navbryce <plunkett.bryce.m@gmail.com>
2021-03-21 15:59:58 -0400

    User and user subdirectory

--- server/utils/ImageUpload.ts
+++ server/utils/ImageUpload.ts
@@ -1,28 +1,47 @@
-import { BlobServiceClient, ContainerClient } from "@azure/storage-blob";
+import { ContainerClient } from "@azure/storage-blob";
+import { SessionUser } from "&server/models/SessionUser";
 
 const sasToken = process.env.NEXT_PUBLIC_STORAGE_SAS_TOKEN; // Fill string with your SAS token
 const containerName = "image-container";
 const storageAccountName =
   process.env.NEXT_PUBLIC_STORAGE_RESOURCE_NAME || "nonprofitportal"; // Fill string with your Storage resource name
 
-export const uploadFileToBlob = async (file: File | null): Promise<string> => {
+/**
+ * Uploads file to /image-container/{user id}/{sub directory}. If a file
+ * already exists on that path, it will be overwritten
+ * @param file
+ * @param user
+ * @param subDirectory
+ */
+export const uploadFileToBlob = async (
+  user: SessionUser,
+  subDirectory: string,
+  file: File | null
+): Promise<string> => {
   if (!file) return "";
-
   const containerClient: ContainerClient = new ContainerClient(
     `https://${storageAccountName}.blob.core.windows.net/${containerName}?${sasToken}`
   );
-  return await createBlobInContainer(containerClient, file);
+  return await createImageBlobInContainer(
+    containerClient,
+    user,
+    subDirectory,
+    file
+  );
 };
 
-const createBlobInContainer = async (
+async function createImageBlobInContainer(
   containerClient: ContainerClient,
+  user: SessionUser,
+  subDirectory: string,
   image: File
-) => {
-  const blobClient = containerClient.getBlockBlobClient(image.name);
+): Promise<string> {
+  const fileName = `${user.id}/${subDirectory}/${image.name}`;
+  const blobClient = containerClient.getBlockBlobClient(fileName);
 
   const options = { blobHTTPHeaders: { blobContentType: image.type } };
 
   await blobClient.uploadBrowserData(image, options);
 
-  return `https://${storageAccountName}.blob.core.windows.net/${containerName}/${image.name}`;
-};
+  return `https://${storageAccountName}.blob.core.windows.net/${fileName}`;
+}
